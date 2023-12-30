15f10f3ca1121275ded22599bb0af7694440cfd6
Brian Masse <brianm25it@gmail.com>
2023-12-29 22:41:04 -0500

    initial commit

--- Sources/UIUniversals/UIUniversals.swift
+++ Sources/UIUniversals/UIUniversals.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct TestView: View {
+    
+    public var body: some View {
+        HStack {
+            Text("hello world!")
+            
+            Image(systemName: "globe.europe.africa")
+        }
+    }
+}
